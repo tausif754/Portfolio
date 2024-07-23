c01ed87a1fc8b339d4411ecf76755ac20ae0e177
Tausif Ahmad <tausifahmad754@gmail.com>
2024-07-23 16:30:48 +0530

    Navbar completed

---
+++ script.js
@@ -0,0 +1,8 @@
+const sideMenu = document.querySelector("#sideMenu");
+
+function openMenu() {
+  sideMenu.style.transform = "translateX(16rem)";
+}
+function closeMenu() {
+  sideMenu.style.transform = "translateX(-16rem)";
+}
